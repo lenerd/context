30e35bfb50ac4a5c5b6abb6f13c3bbd9feb14395
Lennart Braun <lenerd@posteo.de>
2019-10-03 01:15:03 +0200

    XXX: fix ifdef

--- include/boost/context/fiber_ucontext.hpp
+++ include/boost/context/fiber_ucontext.hpp
@@ -270,7 +270,7 @@ public:
                                          (const void **) & from->stack_bottom,
                                          & from->stack_size);
 #endif
-#if defined(BOOST_USE_ASAN)
+#if defined(BOOST_USE_TSAN)
         tsan_fiber_handle = __tsan_create_fiber(0);
 #endif
         Ctx c{ from };
